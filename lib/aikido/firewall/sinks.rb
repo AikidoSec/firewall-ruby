6165976dc16e3f895f8b781b23566564f5187b4f
Nicolas Sanguinetti <foca@foca.io>
2024-08-07 09:26:39 +0200

    PG sink to intercept queries

--- lib/aikido/firewall/sinks.rb
+++ lib/aikido/firewall/sinks.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 require_relative "sinks/mysql2" if defined?(::Mysql2)
+require_relative "sinks/pg" if defined?(::PG)
 require_relative "sinks/trilogy" if defined?(::Trilogy)
