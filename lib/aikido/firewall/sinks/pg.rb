d9f84b40c4771a657696705db51d562309cf1c96
Nicolas Sanguinetti <foca@foca.io>
2024-08-09 10:33:59 +0200

    Raise ActiveRecord::StatementInvalid on all SQLis
    For some reason, the pg adapter does not wrap these exceptions in
    ActiveRecord::StatementInvalid (this is weird, but can't understand
    why yet?), so in order to offer some consistent handling (and make
    it easier to document for users), we're wrapping it ourselves.

--- lib/aikido/firewall/sinks/pg.rb
+++ lib/aikido/firewall/sinks/pg.rb
@@ -11,6 +11,12 @@ module Aikido::Firewall
           def #{method}(query, *)
             Vulnerabilities::SQLInjectionScanner.scan(query, dialect: :postgresql)
             super
+          rescue Aikido::Firewall::SQLInjectionError
+            # The pg adapter does not wrap exceptions in StatementInvalid, which
+            # leads to inconsistent handling. This guarantees that all Aikido
+            # errors are wrapped in a StatementInvalid, so documentation can be
+            # consistent.
+            raise ActiveRecord::StatementInvalid
           end
         RUBY
       end
@@ -22,6 +28,12 @@ module Aikido::Firewall
           def #{method}(_, query, *)
             Vulnerabilities::SQLInjectionScanner.scan(query, dialect: :postgresql)
             super
+          rescue Aikido::Firewall::SQLInjectionError
+            # The pg adapter does not wrap exceptions in StatementInvalid, which
+            # leads to inconsistent handling. This guarantees that all Aikido
+            # errors are wrapped in a StatementInvalid, so documentation can be
+            # consistent.
+            raise ActiveRecord::StatementInvalid
           end
         RUBY
       end
