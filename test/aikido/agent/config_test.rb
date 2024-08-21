955480ce05a2e04b449c340c14f4f1a2312a16f8
Nicolas Sanguinetti <foca@foca.io>
2024-08-21 16:50:05 +0200

    Ensure the server sees some stats quickly
    If the server responds that it hasn't seen any stats, this queues a
    one-off heartbeat event after a (configurable) one minute delay.

--- test/aikido/agent/config_test.rb
+++ test/aikido/agent/config_test.rb
@@ -18,6 +18,8 @@ class Aikido::Agent::ConfigTest < ActiveSupport::TestCase
     assert_kind_of ::Logger, @config.logger
     assert_equal 5000, @config.max_performance_samples
     assert_equal 100, @config.max_compressed_stats
+    assert_equal 60, @config.initial_heartbeat_delay
+    assert_equal 60, @config.polling_interval
   end
 
   test "can overwrite the api_base_url" do
