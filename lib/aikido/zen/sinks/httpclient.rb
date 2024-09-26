162c6e01e6ae174d1420987338765ec7f09b7d56
Nicolas Sanguinetti <foca@foca.io>
2024-09-26 17:31:22 -0300

    Detect SSRFs in the HTTPClient sink

--- lib/aikido/zen/sinks/httpclient.rb
+++ lib/aikido/zen/sinks/httpclient.rb
@@ -7,22 +7,53 @@ module Aikido::Zen
   module Sinks
     module HTTPClient
       SINK = Sinks.add("httpclient", scanners: [
+        Aikido::Zen::Scanners::SSRFScanner,
         Aikido::Zen::OutboundConnectionMonitor
       ])
 
       module Extensions
+        def self.wrap_request(req)
+          Aikido::Zen::HTTP::OutboundRequest.new(
+            verb: req.http_header.request_method,
+            uri: req.http_header.request_uri,
+            headers: req.headers
+          )
+        end
+
+        def self.wrap_response(resp)
+          Aikido::Zen::HTTP::OutboundResponse.new(
+            status: resp.http_header.status_code,
+            headers: resp.headers
+          )
+        end
+
         def do_get_block(req, *)
-          conn = Aikido::Zen::OutboundConnection.from_uri(req.http_header.request_uri)
-          SINK.scan(connection: conn, operation: "request")
+          SINK.scan(
+            connection: Aikido::Zen::OutboundConnection.from_uri(req.http_header.request_uri),
+            request: Extensions.wrap_request(req),
+            operation: "request"
+          )
 
           super
         end
 
         def do_get_stream(req, *)
-          conn = Aikido::Zen::OutboundConnection.from_uri(req.http_header.request_uri)
-          SINK.scan(connection: conn, operation: "request")
+          SINK.scan(
+            connection: Aikido::Zen::OutboundConnection.from_uri(req.http_header.request_uri),
+            request: Extensions.wrap_request(req),
+            operation: "request"
+          )
 
           super
+        end
+
+        def do_get_header(req, res, *)
+          super.tap do
+            Aikido::Zen::Scanners::SSRFScanner.track_redirects(
+              request: Extensions.wrap_request(req),
+              response: Extensions.wrap_response(res)
+            )
+          end
         end
       end
     end
