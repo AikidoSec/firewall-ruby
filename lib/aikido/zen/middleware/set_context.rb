43375264ab4d49f949920f50110159242e2c3581
Nicolas Sanguinetti <foca@foca.io>
2024-11-05 13:55:03 -0300

    Expose the context in the Rack env

--- lib/aikido/zen/middleware/set_context.rb
+++ lib/aikido/zen/middleware/set_context.rb
@@ -3,6 +3,9 @@
 require_relative "../context"
 
 module Aikido::Zen
+  # @!visibility private
+  ENV_KEY = "aikido.context"
+
   module Middleware
     # Rack middleware that keeps the current context in a Thread/Fiber-local
     # variable so that other parts of the agent/firewall can access it.
@@ -14,7 +17,7 @@ module Aikido::Zen
       def call(env)
         context = Context.from_rack_env(env)
 
-        Aikido::Zen.current_context = context
+        Aikido::Zen.current_context = env[ENV_KEY] = context
         Aikido::Zen.track_request(context.request)
 
         @app.call(env)
