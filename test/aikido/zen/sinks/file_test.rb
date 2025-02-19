6c674ce10e5e8255f34391b052418dce615e5429
Pablo Alcantar Morales <pablo@aikido.dev>
2025-02-19 16:52:00 +0100

    split the tests in 3 types: - Normal Execution - Looks Like but it's not attack - Attack Detection

--- test/aikido/zen/sinks/file_test.rb
+++ test/aikido/zen/sinks/file_test.rb
@@ -3,60 +3,71 @@
 require "test_helper"
 
 class Aikido::Zen::Sinks::FileTest < ActiveSupport::TestCase
-  include StubsCurrentContext
-  include SinkAttackHelpers
+  # The following tests validates that the methods still behave as expected even though they are monkey patched
+  class NormalExecutionTest < ActiveSupport::TestCase
+    include StubsCurrentContext
+    include SinkAttackHelpers
 
-  test "scanning does not interfere with `File.read` normally" do
-    Helpers.temp_file "path-traversal-sink-read" do |tmp_file|
-      tmp_file.write "some content"
-      tmp_file.close
-      assert_equal File.read(tmp_file.path), "some content"
-    end
-  end
-
-  test "scanning does not interfere with `File.write` normally" do
-    path = Helpers.temp_file_name "path-traversal-sink-write"
-    File.write path, "path-traversal-sink-write"
-
-    assert_equal File.read(path), "path-traversal-sink-write"
-    File.unlink path
-  end
-
-  test "scanning does not interfere with `File.join` normally" do
-    assert_equal File.join("base"), "base"
-    assert_equal File.join("base", "some"), "base/some"
-    assert_equal File.join("base", "some", "path"), "base/some/path"
-  end
-
-  test "scanning does not interfere with `File.chmod` normally" do
-    Helpers.temp_file "path-traversal-sink-chmod" do |tmp_file|
-      assert_equal File.chmod(0o755, tmp_file.path), 1
-    end
-  end
-
-  test "does not fail when the context is null but looks like an attack" do
-    # We expect the next `File.(read|write)`  will fail *because* the file does not exist,
-    # but *not because* it is Path Traversal Attack
-    refute_attack do
-      assert_raise Errno::ENOENT do
-        File.read("../looks-like-an-attack")
+    test "File.read" do
+      Helpers.temp_file "path-traversal-sink-read" do |tmp_file|
+        tmp_file.write "some content"
+        tmp_file.close
+        assert_equal File.read(tmp_file.path), "some content"
       end
     end
 
-    refute_attack do
-      path = Helpers.temp_file_name
-      assert_raise Errno::ENOENT do
-        File.write path + "/../looks-like-an-attack", "content"
-      end
+    test "File.write" do
+      path = Helpers.temp_file_name "path-traversal-sink-write"
+      File.write path, "path-traversal-sink-write"
+
+      assert_equal File.read(path), "path-traversal-sink-write"
+      File.unlink path
     end
 
-    # Check for `File.join`
-    refute_attack do
-      assert_equal File.join("base", "some", "/../", "looks-like-an-attack"), "base/some/../looks-like-an-attack"
+    test "File.join" do
+      assert_equal File.join("base"), "base"
+      assert_equal File.join("base", "some"), "base/some"
+      assert_equal File.join("base", "some", "path"), "base/some/path"
+    end
+
+    test "File.chmod" do
+      Helpers.temp_file "path-traversal-sink-chmod" do |tmp_file|
+        assert_equal File.chmod(0o755, tmp_file.path), 1
+      end
     end
   end
 
-  class AttacksDetectionTest < ActiveSupport::TestCase
+  # The following tests are have a null `Context`, that means although they _might be_ attacks, they won't be
+  # considered as such by the scanner
+  class LookLikeAttackTest < ActiveSupport::TestCase
+    include StubsCurrentContext
+    include SinkAttackHelpers
+
+    test "File.read" do
+      refute_attack do
+        assert_raise Errno::ENOENT do
+          File.read("../looks-like-an-attack")
+        end
+      end
+    end
+
+    test "File.write" do
+      refute_attack do
+        path = Helpers.temp_file_name
+        assert_raise Errno::ENOENT do
+          File.write path + "/../looks-like-an-attack", "content"
+        end
+      end
+    end
+
+    test "File.join" do
+      refute_attack do
+        assert_equal File.join("base", "some", "/../", "looks-like-an-attack"), "base/some/../looks-like-an-attack"
+      end
+    end
+  end
+
+  class AttackDetectionTest < ActiveSupport::TestCase
     include StubsCurrentContext
     include SinkAttackHelpers
 
@@ -72,25 +83,19 @@ class Aikido::Zen::Sinks::FileTest < ActiveSupport::TestCase
         "Path Traversal: Malicious user input «#{OFFENDER_PATH}» detected while calling method #{operation}"
     end
 
-    test "File.read" do
+    test "attacks are detected by the scanner" do
       assert_path_traversal_attack "File.read" do
         File.read OFFENDER_PATH
       end
-    end
 
-    test "File.write" do
       assert_path_traversal_attack "File.write" do
         File.write OFFENDER_PATH, "content"
       end
-    end
 
-    test "File.join" do
       assert_path_traversal_attack "File.join" do
         File.join "some", "path", OFFENDER_PATH
       end
-    end
 
-    test "File.chmod" do
       assert_path_traversal_attack "File.chmod" do
         File.chmod 0o755, OFFENDER_PATH
       end
