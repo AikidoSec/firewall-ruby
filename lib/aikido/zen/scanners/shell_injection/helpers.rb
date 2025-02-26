2da423eb6a6d5a2268a23eb6e96b4a730df8e303
Pablo Alcantar Morales <pablo@aikido.dev>
2025-02-26 09:10:05 +0100

    bettern organization

--- lib/aikido/zen/scanners/shell_injection/helpers.rb
+++ lib/aikido/zen/scanners/shell_injection/helpers.rb
@@ -4,6 +4,20 @@ module Aikido::Zen::Scanners::ShellInjectionScanner
   module Helpers
     ESCAPE_CHARS = %W[' "]
     DANGEROUS_CHARS_INSIDE_DOUBLE_QUOTES = %W[$ ` \\ !]
+    DANGEROUS_CHARS = [
+      "#", "!", '"', "$", "&", "'", "(", ")", "*", ";", "<", "=", ">", "?",
+      "[", "\\", "]", "^", "`", "{", "|", "}", " ", "\n", "\t", "~"
+    ]
+
+    COMMANDS = %w[sleep shutdown reboot poweroff halt ifconfig chmod chown ping
+      ssh scp curl wget telnet kill killall rm mv cp touch echo cat head
+      tail grep find awk sed sort uniq wc ls env ps who whoami id w df du
+      pwd uname hostname netstat passwd arch printenv logname pstree hostnamectl
+      set lsattr killall5 dmesg history free uptime finger top shopt :]
+
+    PATH_PREFIXES = %w[/bin/ /sbin/ /usr/bin/ /usr/sbin/ /usr/local/bin/ /usr/local/sbin/]
+
+    SEPARATORS = [" ", "\t", "\n", ";", "&", "|", "(", ")", "<", ">"]
 
     def self.is_safely_encapsulated(command, user_input)
       command.split(user_input).each_cons(2) do |first_segment, second_segment|
@@ -29,8 +43,9 @@ module Aikido::Zen::Scanners::ShellInjectionScanner
       end
     end
 
-    def self.contains_shell_syntax(command, input)
-      # code here
+    # Helper function for sorting commands by length (longer commands first)
+    def self.by_length(a, b)
+      b.length - a.length
     end
 
     # Escape characters with special meaning either inside or outside character sets.
@@ -40,6 +55,60 @@ module Aikido::Zen::Scanners::ShellInjectionScanner
     # Inspired by https://github.com/sindresorhus/escape-string-regexp/
     def self.escape_string_regexp(string)
       string.gsub(/[|\\{}()\[\]^$+*?.]/) { "\\#{$&}" }.gsub("-", '\\x2d')
+    end
+
+    # Construct the regex for commands
+    COMMANDS_REGEX = Regexp.new(
+      "([/.]*((#{PATH_PREFIXES.map { |p| Helpers.escape_string_regexp(p) }.join("|")})?((#{COMMANDS.sort(&method(:by_length)).join("|")}))))",
+      Regexp::IGNORECASE
+    )
+
+    def self.contains_shell_syntax(command, user_input)
+      # Check if input is only whitespace
+      return false if user_input.strip.empty?
+
+      # Check if the user input contains any dangerous characters
+      if DANGEROUS_CHARS.any? { |c| user_input.include?(c) }
+        return true
+      end
+
+      # If the command is exactly the same as the user input, check if it matches the regex
+      if command == user_input
+        return match_all(command, COMMANDS_REGEX).any? do |match|
+          match[0].length == command.length && match[0] == command
+        end
+      end
+
+      # Check if the command contains a commonly used command
+      match_all(command, COMMANDS_REGEX).each do |match|
+        next if user_input != match[0]
+
+        # Check if the command is surrounded by separators
+        char_before = command[match[0].index - 1]
+        char_after = command[match[0].index + match[0].length]
+
+        if SEPARATORS.include?(char_before) && SEPARATORS.include?(char_after)
+          return true
+        end
+
+        if SEPARATORS.include?(char_before) && char_after.nil?
+          return true
+        end
+
+        if char_before.nil? && SEPARATORS.include?(char_after)
+          return true
+        end
+      end
+
+      false
+    end
+
+    def self.match_all(str, regex)
+      matches = []
+      str.scan(regex) do |match|
+        matches << match
+      end
+      matches
     end
   end
 end
--- test/aikido/zen/scanners/shell_injection/helpers_test.rb
+++ test/aikido/zen/scanners/shell_injection/helpers_test.rb
@@ -11,6 +11,14 @@ class Aikido::Zen::Scanners::ShellInjectionScanner::HelpersTest < ActiveSupport:
     refute Aikido::Zen::Scanners::ShellInjectionScanner::Helpers.is_safely_encapsulated(command, user_input)
   end
 
+  def assert_contains_shell_syntax(command, command2 = command)
+    assert Aikido::Zen::Scanners::ShellInjectionScanner::Helpers.contains_shell_syntax(command, command2)
+  end
+
+  def refute_contains_shell_syntax(command, command2 = command)
+    refute Aikido::Zen::Scanners::ShellInjectionScanner::Helpers.contains_shell_syntax(command, command2)
+  end
+
   test "safe between single quotes" do
     assert_is_safely_encapsulated("echo '$USER'", "$USER")
     assert_is_safely_encapsulated("echo '`$USER'", "`USER")
@@ -66,5 +74,34 @@ class Aikido::Zen::Scanners::ShellInjectionScanner::HelpersTest < ActiveSupport:
   test "escapes - in a way compatible with the Unicode flag" do
     regex = Regexp.new(Aikido::Zen::Scanners::ShellInjectionScanner::Helpers.escape_string_regexp("-"), Regexp::MULTILINE)
     assert_match regex, "-"
+  end
+
+  test "detects shell syntax" do
+    refute_contains_shell_syntax ""
+    refute_contains_shell_syntax "hello"
+    refute_contains_shell_syntax "\n"
+    refute_contains_shell_syntax "\n\n"
+
+    assert_contains_shell_syntax "$(command)"
+    assert_contains_shell_syntax "$(command arg arg)"
+    assert_contains_shell_syntax "`command`"
+    assert_contains_shell_syntax "\narg"
+    assert_contains_shell_syntax "\targ"
+    assert_contains_shell_syntax "\narg\n"
+    assert_contains_shell_syntax "arg\n"
+    assert_contains_shell_syntax "arg\narg"
+    assert_contains_shell_syntax "rm -rf"
+    assert_contains_shell_syntax "/bin/rm -rf"
+    assert_contains_shell_syntax "/bin/rm"
+    assert_contains_shell_syntax "/sbin/sleep"
+    assert_contains_shell_syntax "/usr/bin/kill"
+    assert_contains_shell_syntax "/usr/bin/killall"
+    assert_contains_shell_syntax "/usr/bin/env"
+    assert_contains_shell_syntax "/bin/ps"
+    assert_contains_shell_syntax "/usr/bin/W"
+  end
+
+  test "it detects commands surrounded by separators" do
+    assert_contains_shell_syntax 'find /path/to/search -type f -name "pattern" -exec rm {} \\\\;', "rm"
   end
 end
