090983c777871ad3e9c45f79c83f0c85acb630aa
Pablo Alcantar Morales <pablo@aikido.dev>
2025-03-19 13:03:06 +0100

    dRB server start attempts capped to 20

--- lib/aikido/zen/detached_agent.rb
+++ lib/aikido/zen/detached_agent.rb
@@ -3,22 +3,26 @@
 require "drb/drb"
 
 module Aikido::Zen
+  # It's possible to use `extend Forwardable` here for one-line forward calls to the
+  # @detached_agent_front object. Unfortunately, the methods to be called are
+  # created at runtime by `DRbObject`, which leads to an ugly warning about
+  # private methods after the delegator is bound.
   class DetachedAgent
-    extend Forwardable
-
-    def_delegators :@detached_agent_front, :middleware_installed!
-
     def initialize(config: Aikido::Zen.config)
       @config = config
-      @detached_agent_front = DRbObject.new(nil, config.detached_agent_socket_path)
+      @detached_agent_front = DRbObject.new_with_uri(config.detached_agent_socket_path)
     end
 
     def track_request(request)
       @detached_agent_front.track_request
     end
 
-    # Everytime a fork happens, we have to start a DRb service in a background thread
-    # it will be in charge of maintaining the connection and clean up resources.
+    def middleware_installed!
+      @detached_agent_front.middleware_installed!
+    end
+
+    # Everytime a fork happens, we have to start a DRb service in a background thread.
+    # It will be in charge of maintaining the connection and clean up resources.
     def handle_fork
       DRb.start_service
     end
@@ -40,18 +44,23 @@ module Aikido::Zen
       @config = config
       @detached_agent_front = DetachedAgentFront.new
       @drb_server = DRb.start_service(config.detached_agent_socket_path, @detached_agent_front)
-      @drb_server.verbose = true
+      @drb_server.verbose = @config.logger.debug?
+      @max_attempts = 10
     end
 
     def start
+      attempts = 0
       until @drb_server.alive?
-        @config.logger.info("server still not alive")
-        sleep 0.3
+        @config.logger.info("DRb Server still not alive. #{@max_attempts - attempts} attempts remaining")
+        sleep 0.1
+        attempts += 1
+        raise DetachedAgentError.new("Impossible to start the dRB server (socket=#{@config.detached_agent_socket_path})") if attempts == @max_attempts
       end
     end
 
     def stop!
       @drb_server.stop_service
+      DRb.stop_service
     end
   end
 end
