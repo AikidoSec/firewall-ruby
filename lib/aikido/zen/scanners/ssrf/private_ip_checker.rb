647f7d840089bdf627d9865b48b9db945a57ae56
Timo Kössler <info@timokoessler.de>
2025-03-24 11:10:05 +0100

    Improve private ip detection

--- lib/aikido/zen/scanners/ssrf/private_ip_checker.rb
+++ lib/aikido/zen/scanners/ssrf/private_ip_checker.rb
@@ -31,34 +31,45 @@ module Aikido::Zen
         # @return [Boolean]
         def private?(hostname_or_address)
           resolve(hostname_or_address).any? do |ip|
-            ip.loopback? || ip.private? || RFC5735.any? { |range| range === ip }
+            PRIVATE_RANGES.any? { |range| range === ip }
           end
         end
 
         private
 
-        RFC5735 = [
-          IPAddr.new("0.0.0.0/8"),
-          IPAddr.new("100.64.0.0/10"),
-          IPAddr.new("127.0.0.0/8"),
-          IPAddr.new("169.254.0.0/16"),
-          IPAddr.new("192.0.0.0/24"),
-          IPAddr.new("192.0.2.0/24"),
-          IPAddr.new("192.31.196.0/24"),
-          IPAddr.new("192.52.193.0/24"),
-          IPAddr.new("192.88.99.0/24"),
-          IPAddr.new("192.175.48.0/24"),
-          IPAddr.new("198.18.0.0/15"),
-          IPAddr.new("198.51.100.0/24"),
-          IPAddr.new("203.0.113.0/24"),
-          IPAddr.new("240.0.0.0/4"),
-          IPAddr.new("224.0.0.0/4"),
-          IPAddr.new("255.255.255.255/32"),
-
-          IPAddr.new("::/128"),              # Unspecified address
-          IPAddr.new("fe80::/10"),           # Link-local address (LLA)
-          IPAddr.new("::ffff:127.0.0.1/128") # IPv4-mapped address
+        PRIVATE_IPV4_RANGES = [
+          IPAddr.new("0.0.0.0/8"),         # "This" network (RFC 1122)
+          IPAddr.new("10.0.0.0/8"),        # Private-Use Networks (RFC 1918)
+          IPAddr.new("100.64.0.0/10"),     # Shared Address Space (RFC 6598)
+          IPAddr.new("127.0.0.0/8"),       # Loopback (RFC 1122)
+          IPAddr.new("169.254.0.0/16"),    # Link Local (RFC 3927)
+          IPAddr.new("172.16.0.0/12"),     # Private-Use Networks (RFC 1918)
+          IPAddr.new("192.0.0.0/24"),      # IETF Protocol Assignments (RFC 5736)
+          IPAddr.new("192.0.2.0/24"),      # TEST-NET-1 (RFC 5737)
+          IPAddr.new("192.31.196.0/24"),   # AS112 Redirection Anycast (RFC 7535)
+          IPAddr.new("192.52.193.0/24"),   # Automatic Multicast Tunneling (RFC 7450)
+          IPAddr.new("192.88.99.0/24"),    # 6to4 Relay Anycast (RFC 3068)
+          IPAddr.new("192.168.0.0/16"),    # Private-Use Networks (RFC 1918)
+          IPAddr.new("192.175.48.0/24"),   # AS112 Redirection Anycast (RFC 7535)
+          IPAddr.new("198.18.0.0/15"),     # Network Interconnect Device Benchmark Testing (RFC 2544)
+          IPAddr.new("198.51.100.0/24"),   # TEST-NET-2 (RFC 5737)
+          IPAddr.new("203.0.113.0/24"),    # TEST-NET-3 (RFC 5737)
+          IPAddr.new("224.0.0.0/4"),       # Multicast (RFC 3171)
+          IPAddr.new("240.0.0.0/4"),       # Reserved for Future Use (RFC 1112)
+          IPAddr.new("255.255.255.255/32") # Limited Broadcast (RFC 919)
         ]
+
+        PRIVATE_IPV6_RANGES = [
+          IPAddr.new("::/128"),            # Unspecified address (RFC 4291)
+          IPAddr.new("::1/128"),           # Loopback address (RFC 4291)
+          IPAddr.new("fc00::/7"),          # Unique local address (ULA) (RFC 4193
+          IPAddr.new("fe80::/10"),         # Link-local address (LLA) (RFC 4291)
+          IPAddr.new("100::/64"),          # Discard prefix (RFC 6666)
+          IPAddr.new("2001:db8::/32"),     # Documentation prefix (RFC 3849)
+          IPAddr.new("3fff::/20")          # Documentation prefix (RFC 9637)
+        ]
+
+        PRIVATE_RANGES = PRIVATE_IPV4_RANGES + PRIVATE_IPV6_RANGES + PRIVATE_IPV4_RANGES.map(&:ipv4_mapped)
 
         def resolved_in_current_context
           context = Aikido::Zen.current_context
