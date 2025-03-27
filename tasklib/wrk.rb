2801814554d8ce8855ef704dc218cbc1a9a1c6b3
Pablo Alcantar Morales <pablo@aikido.dev>
2025-03-27 11:58:41 +0100

    tune in benchmark params

--- tasklib/wrk.rb
+++ tasklib/wrk.rb
@@ -1,9 +1,11 @@
 require "open3"
 require "time"
 
+NUMBER_OF_THREADS = ENV.fetch("BENCHMARK_NUMBER_OF_THREADS") { "12" }.to_s
+
 def generate_wrk_command_for_url(url)
   # Define the command with wrk included
-  "wrk --threads 5 --connections 200 --duration 15s --timeout 5s #{url}"
+  "wrk --threads #{NUMBER_OF_THREADS} --connections 100 --duration 15s --timeout 5s --latency #{url}"
 end
 
 def cold_start(url)
@@ -49,7 +51,7 @@ def run_benchmark(route_no_zen:, route_zen:, description:, throughput_decrease_l
   puts <<~MSG
     WRK OUTPUT
     ================
-      FIREWALL ENABLED:
+    FIREWALL ENABLED:
         #{out}
     ----------------
   MSG
@@ -57,7 +59,7 @@ def run_benchmark(route_no_zen:, route_zen:, description:, throughput_decrease_l
 
   out, err, status = Open3.capture3(generate_wrk_command_for_url(route_no_zen))
   puts <<~MSG
-      FIREWALL DISABLED:
+    FIREWALL DISABLED:
         #{out}
     ================
   MSG
