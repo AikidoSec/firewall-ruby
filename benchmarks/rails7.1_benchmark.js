3fe5a61cd63a157747a0652bfc3f16a2c7e416e2
Pablo Alcantar Morales <pablo@aikido.dev>
2025-03-31 16:01:17 +0200

    k6: - don't measure when zen is faster -> it means that the server is congested - use k6 tags instead of different metrics

--- benchmarks/rails7.1_benchmark.js
+++ benchmarks/rails7.1_benchmark.js
@@ -1,36 +1,39 @@
 import http from 'k6/http';
-import {Trend} from 'k6/metrics';
+import {Trend, Counter} from 'k6/metrics';
 
 const HTTP = {
   withZen: {
-    get: (path, ...args) => http.get("http://localhost:3001" + path, ...args),
-    post: (path, ...args) => http.post("http://localhost:3001" + path, ...args)
+    get: (path, ...args) => http.get("http://localhost:3001" + path, {tags: {zen_enabled: true}, ...args}),
+    post: (path, ...args) => http.post("http://localhost:3001" + path, {tags: {zen_enabled: true}, ...args}),
   },
+
   withoutZen: {
-    get: (path, ...args) => http.get("http://localhost:3002" + path, ...args),
-    post: (path, ...args) => http.post("http://localhost:3002" + path, ...args)
+    get: (path, ...args) => http.get("http://localhost:3002" + path, {tags: {zen_enabled: false}, ...args}),
+    post: (path, ...args) => http.post("http://localhost:3002" + path, {tags: {zen_enabled: false}, ...args}),
   }
 }
 
 function test(name, fn) {
   const withZen = fn(HTTP.withZen);
   const withoutZen = fn(HTTP.withoutZen);
-  const timeWithZen = withZen.timings.duration;
-  const timeWithoutZen = withoutZen.timings.duration;
+  const timeWithZen = withZen.timings.waiting;
+  const timeWithoutZen = withoutZen.timings.waiting;
 
-  tests[name].delta.add(timeWithZen - timeWithoutZen);
-  tests[name].overhead.add(100 * (timeWithZen - timeWithoutZen) / timeWithoutZen)
-
-  tests[name].with_zen.add(timeWithZen);
-  tests[name].without_zen.add(timeWithoutZen);
+  if (timeWithZen - timeWithoutZen > 0) {
+    tests[name].delta.add(timeWithZen - timeWithoutZen);
+    tests[name].overhead.add(100 * (timeWithZen - timeWithoutZen) / timeWithoutZen)
+  } else {
+    tests[name].zen_fastest.add(1)
+    tests[name].zen_fastest_trend.add(timeWithoutZen - timeWithZen);
+  }
 }
 
 function buildTestTrends(prefix) {
   return {
-    delta: new Trend(`${prefix}_delta`),
-    with_zen: new Trend(`${prefix}_with_zen`),
-    without_zen: new Trend(`${prefix}_without_zen`),
-    overhead: new Trend(`${prefix}_overhead`)
+    delta: new Trend(`${prefix}_delta`, true),
+    overhead: new Trend(`${prefix}_overhead`),
+    zen_fastest: new Counter(`${prefix}_zen_fastest`),
+    zen_fastest_trend: new Trend(`${prefix}_zen_fastest_trend`, true),
   };
 }
 
@@ -38,12 +41,16 @@ const tests = {
   benchmark_page: buildTestTrends("benchmark_page"),
 }
 export const options = {
-  vus: 10, // Number of virtual users
-  duration: '30s',
-  // iterations: 2000,
+  vus: 4, // Number of virtual users
+  duration: '15s',
   thresholds: {
     http_req_failed: ['rate==0'], // we are marking the attacks as expected, so we should have no errors
-    benchmark_page_delta: ["med<10"],
+    "http_req_duration{zen_enabled:true}": ["p(95)<10"],
+    "http_req_duration{zen_enabled:false}": ["p(95)<10"],
+
+    // Our endpoint is sleep by 1ms, ensuring
+    "http_req_waiting{zen_enabled:false}": ["p(95)<3", "med<2"],
+    "http_req_waiting{zen_enabled:true}": ["p(95)<4", "med<3"],
   }
 };
 
