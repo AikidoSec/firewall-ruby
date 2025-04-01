f2b274a55f933a7b266c4dcc846652ca4eeb5691
Pablo Alcantar Morales <pablo@aikido.dev>
2025-04-01 11:02:22 +0200

    don't use a background worker, Instead made the detached_agent work as a normal agent

--- lib/aikido/zen/detached_agent/agent.rb
+++ lib/aikido/zen/detached_agent/agent.rb
@@ -11,68 +11,39 @@ module Aikido::Zen::DetachedAgent
   # created at runtime by `DRbObject`, which leads to an ugly warning about
   # private methods after the delegator is bound.
   class Agent
-    def initialize(config: Aikido::Zen.config)
+    def initialize(
+      config: Aikido::Zen.config,
+      collector: Aikido::Zen.collector,
+      worker: Aikido::Zen::Worker.new(config: config)
+    )
       @config = config
+      @worker = worker
+      @collector = collector
       @detached_agent_front = DRbObject.new_with_uri(config.detached_agent_socket_path)
-      @background_worker = Aikido::Zen::BackgroundWorker.new do |unit_work|
-        method, args = unit_work
 
-        # Performance: This code could have been created using Procs or calling
-        # `send(method, arg)` on @detached_agent_object, but using that kind of code,
-        # is less performant than having explicit code to check each case.
-        case method
-        when :track_request then @detached_agent_front.track_request
-        when :track_route then @detached_agent_front.track_route(args[0], args[1])
-        when :track_outbound then @detached_agent_front.track_outbound(args[0])
-        when :track_scan then @detached_agent_front.track_scan(args[0], args[1], args[2])
-        when :track_user then @detached_agent_front.track_user(args[0], args[1], args[2], args[3])
-        when :track_attack then @detached_agent_front.track_attack(args[0], args[1])
-        else raise Error("Unrecognized method #{method}")
-        end
-      rescue => e
-        @config.logger.error(e)
-      end
-
-      @background_worker.start
+      schedule_tasks
     end
 
-    def track_request
-      @background_worker.enqueue([:track_request])
+    def send_heartbeat(at: Time.now.utc)
+      return unless @collector.stats.any?
+
+      heartbeat = @collector.flush(at: at)
+      @config.logger.debug("Sending heartbeat from child [#{Process.pid}] to parent process [#{Process.ppid}]")
+      @detached_agent_front.send_heartbeat_to_parent_process(heartbeat.to_json)
     end
 
-    def middleware_installed!
-      @detached_agent_front.middleware_installed!
-    end
-
-    def track_route(request)
-      # schema is a complex object, which must be converted to a hash outside the enqueue ->()
-      # otherwise it might GC-ed.
-      schema = request.schema.as_json
-      @background_worker.enqueue([:track_route, [request.route, schema]])
-    end
-
-    def track_outbound(outbound)
-      @background_worker.enqueue([:track_outbound, [outbound]])
-    end
-
-    def track_scan(scan)
-      @background_worker.enqueue([:track_scan, [scan.sink.name, scan.errors?, scan.duration]])
-    end
-
-    def track_user(user)
-      @background_worker.enqueue([:track_user, [user.id, user.name, user.first_seen_at, user.ip]])
-    end
-
-    def track_attack(attack)
-      @background_worker.enqueue([:track_attack, [attack.sink.name, attack.blocked?]])
+    private def schedule_tasks
+      @worker.every(10, run_now: false) { send_heartbeat }
     end
 
     # Every time a fork occurs (a new child process is created), we need to start
     # a DRb service in a background thread within the child process. This service
     # will manage the connection and handle resource cleanup.
     def handle_fork
-      @background_worker.restart
       DRb.start_service
+      @worker.shutdown
+      @worker = Aikido::Zen::Worker.new(config: @config)
+      schedule_tasks
     end
   end
 end
