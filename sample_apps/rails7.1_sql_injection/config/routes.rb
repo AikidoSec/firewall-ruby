2863bd9e67714d46fff608856bcec5f9c3b87942
Pablo Alcantar Morales <pablo@aikido.dev>
2025-04-01 09:45:47 +0200

    new endpoint to test with an user

--- sample_apps/rails7.1_sql_injection/config/routes.rb
+++ sample_apps/rails7.1_sql_injection/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", :as => :rails_health_check
 
   get "benchmark", controller: "benchmark", action: "index"
+  get "benchmark_with_user", controller: "benchmark", action: "with_user"
 
   # Defines the root path route ("/")
   # root "posts#index"
