8fdb787b421d9ca0a79ab38c2915ee0fc361742e
Mark Smith <mark@aikido.dev>
2025-06-02 17:05:40 +0200

    Replace version using release tag name

--- lib/aikido/zen/version.rb
+++ lib/aikido/zen/version.rb
@@ -2,7 +2,7 @@
 
 module Aikido
   module Zen
-    VERSION = "0.1.1"
+    VERSION = "1.0-REPLACE-VERSION"
 
     # The version of libzen_internals that we build against.
     LIBZEN_VERSION = "0.1.37"
