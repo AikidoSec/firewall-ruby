8b0c19f81c9c5690c4e9ad478367eb2581d1822f
Mark Smith <mark@aikido.dev>
2025-06-04 09:39:41 +0200

    Revert "Merge pull request #108 from AikidoSec/replace-version-using-release-tag-name"
    This reverts commit 7fa20e5480e0e2dacb4923e4b8efa0cacc9b7e44, reversing
    changes made to 6248e6ef9bad83d7aa7062bc2113226644c3308b.

--- lib/aikido/zen/version.rb
+++ lib/aikido/zen/version.rb
@@ -2,7 +2,7 @@
 
 module Aikido
   module Zen
-    VERSION = "1.0-REPLACE-VERSION"
+    VERSION = "0.1.1"
 
     # The version of libzen_internals that we build against.
     LIBZEN_VERSION = "0.1.37"
