417128c9e6e3211c665815f3c9627e7b42287b8c
Mark Smith <mark@aikido.dev>
2025-07-08 10:49:16 +0200

    Add global comment

--- lib/aikido/zen/sinks_dsl.rb
+++ lib/aikido/zen/sinks_dsl.rb
@@ -5,6 +5,66 @@ module Aikido::Zen
     module DSL
       extend self
 
+      # In the context of `Aikido::Zen::Sinks::DSL`, the terms safe and presafe
+      # are defined as follows:
+      #
+      # safe: the desired state for a sink, particularly with respect to rescue.
+      #
+      # A sink is considered safe when unintended errors in the sink are handled,
+      # and-so are prevented from disrupting the operation of the original method
+      # (by raising unintended errors).
+      #
+      # presafe: the default state of a sink, particularly with respect to rescue.
+      #
+      # A sink is in the presafe state before and while unintended errors in the
+      # sink are not handled.
+      #
+      # Sink methods (like all methods) are in the presafe state when defined and
+      # become safe when unexpected errors cannot cause harm. The `safe` method
+      # is used to establish a safe state for the duration of the block executed.
+      # It is sometimes useful to be able to reestablish the presafe safe while
+      # inside a `safe` block; the `presafe` method allows this.
+      #
+      # Methods that contain the term presafe in their name should be used with
+      # appropriate care and understanding.
+      #
+      # IMPORTANT: All sinks should be safe!
+      #
+      # While this DSL proves useful for defining safe sink methods that follow
+      # common patterns, there are exceptions. It is always possible to define
+      # sink methods without using this DSL, but this should only be done when
+      # absolutely necessary. The sink methods defined using this DSL are safe,
+      # unless explicitly declared presafe.
+      #
+      # IMPORTANT: No sinks should be presafe in production!
+      #
+      # We are all responsible for ensuring that the sinks we implement are safe
+      # for production use. This DSL is only here to assist, by taking care of
+      # delicate edge cases and reducing the space for errors.
+      #
+      # When writing sink methods manually, some principles should be considered,
+      # to ensure safety for production:
+      #
+      # 1. Sink methods should ensure that the original method is always called,
+      # passing all parameters (positional, keyword, and block) exactly as they
+      # were passed to the original method, and return the result returned by
+      # the original method exactly as it was returned by the original method.
+      # (Unless intervention is required.)
+      #
+      # 2. Sink methods should not predict the signature of the original method,
+      # and-so restrict it from varying. The original method implementation is
+      # the sole and ultimate reference for its own behavior. We are observers
+      # (unless intervention is required).
+      #
+      # 3. Unexpected errors that are encountered in sink methods should not be
+      # capable of interfering with or preventing the normal operation of the
+      # original method. This includes but is not limited to exceptions that
+      # that may be raised when the sink method is called. Safe sink methods
+      # should return control to their caller (unless intervention is required).
+      #
+      # These are the guidelines adhered to by the `Aikido::Zen::Sinks::DSL`.
+
+      # The error with an original error as its cause to re-raise in `safe`.
       class PresafeError < StandardError
       end
 
