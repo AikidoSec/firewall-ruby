f48e8486d415c964b2bda078ce5949d7aa1275f6
Mark Smith <mark.smith@farthing.dev>
2025-09-09 10:34:06 +0200

    Bump version to 1.0.2.beta.5
    Failed to publish v1.0.2.beta.4 to RubyGems.org.

--- lib/aikido/zen/version.rb
+++ lib/aikido/zen/version.rb
@@ -2,7 +2,7 @@
 
 module Aikido
   module Zen
-    VERSION = "1.0.2.beta.4"
+    VERSION = "1.0.2.beta.5"
 
     # The version of libzen_internals that we build against.
     LIBZEN_VERSION = "0.1.39"
