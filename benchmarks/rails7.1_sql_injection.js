765d7fa7e7e78bfa7e35b057d33f3424572c9b31
Mark Smith <mark@aikido.dev>
2025-10-22 11:25:33 +0200

    Make the k6 benchmark more realistic

--- benchmarks/rails7.1_sql_injection.js
+++ benchmarks/rails7.1_sql_injection.js
@@ -1,5 +1,5 @@
-import http from 'k6/http';
-import {Trend} from 'k6/metrics';
+import http from "k6/http";
+import {Trend} from "k6/metrics";
 
 const HTTP = {
   withZen: {
@@ -10,7 +10,7 @@ const HTTP = {
     get: (path, ...args) => http.get("http://localhost:3002" + path, ...args),
     post: (path, ...args) => http.post("http://localhost:3002" + path, ...args)
   }
-}
+};
 
 function test(name, fn) {
   const withZen = fn(HTTP.withZen);
@@ -36,35 +36,67 @@ function buildTestTrends(prefix) {
 
 const tests = {
   test_post_page_with_json_body: buildTestTrends("test_post_page_with_json_body"),
-  test_get_page_without_attack: buildTestTrends("test_get_page_without_attack"),
-  test_get_page_with_sql_injection: buildTestTrends("test_get_page_with_sql_injection")
-}
+  test_get_page_without_attack: buildTestTrends("test_get_page_without_attack")
+};
+
 export const options = {
   vus: 1, // Number of virtual users
-  iterations: 200,
+  duration: "60s",
   thresholds: {
-    http_req_failed: ['rate==0'], // we are marking the attacks as expected, so we should have no errors
+    http_req_failed: ["rate==0"], // We are marking the attacks as expected, so we should have no errors.
     test_post_page_with_json_body_delta: ["med<10"],
-    test_get_page_without_attack_delta: ["med<10"],
-    test_get_page_with_sql_injection_delta: ["med<10"],
+    test_get_page_without_attack_delta: ["med<10"]
   }
+};
+
+const headers = {
+  Authorization:
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.Bw8sSk3kdnT9d803kqqE_LZJzY1PzMl5cbmuanQKxrI",
+  Accept:
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+  "Accept-Language": "en-US,en;q=0.9",
+  Dnt: "1",
+  Priority: "u=0, i",
+  "Sec-Ch-Ua":
+    '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
+  "Sec-Ch-Ua-Arch": '"arm"',
+  "Sec-Ch-Ua-Bitness": '"64"',
+  "Sec-Ch-Ua-Full-Version-List":
+    '"Not)A;Brand";v="99.0.0.0", "Google Chrome";v="127.0.6533.72", "Chromium";v="127.0.6533.72"',
+  "Sec-Ch-Ua-Mobile": "?0",
+  "Sec-Ch-Ua-Model": '""',
+  "Sec-Ch-Ua-Platform": '"macOS"',
+  "Sec-Ch-Ua-Platform-Version": '"14.5.0"',
+  "Sec-Ch-Ua-Wow64": "?0",
+  "Sec-Fetch-Dest": "document",
+  "Sec-Fetch-Mode": "navigate",
+  "Sec-Fetch-Site": "cross-site",
+  "Sec-Fetch-User": "?1",
+  "Sec-Gpc": "1",
+  "Upgrade-Insecure-Requests": "1",
+  "User-Agent":
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
 };
 
 const expectAttack = http.expectedStatuses(200, 500);
 
 export default function () {
   test("test_post_page_with_json_body",
-    (http) => http.post("/cats", JSON.stringify({cat: {name: "Féline Dion"}}), {
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      }
-    })
+    (http) => http.post("/cats",
+      JSON.stringify({cat: {name: "Féline Dion"}}),
+      {
+        headers: {
+          ...headers,
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        }
+      })
   )
 
-  test("test_get_page_without_attack", (http) => http.get("/cats"))
-
-  test("test_get_page_with_sql_injection", (http) =>
-    http.get("/cats/1'%20OR%20''='", { responseCallback: expectAttack })
+  test("test_get_page_without_attack",
+    (http) => http.get("/cats",
+    {
+        headers: headers
+    })
   )
 }
