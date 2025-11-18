21e955cc554fcce6c45f3a5f4b37e1e15069d5b4
Mark Smith <mark@aikido.dev>
2025-11-18 14:04:53 +0100

    Add LRU mode capped collections

--- lib/aikido/zen/capped_collections.rb
+++ lib/aikido/zen/capped_collections.rb
@@ -18,8 +18,8 @@ module Aikido::Zen
     # @return [Integer]
     attr_reader :capacity
 
-    def initialize(capacity)
-      @data = CappedMap.new(capacity)
+    def initialize(capacity, mode: :fifo)
+      @data = CappedMap.new(capacity, mode: mode)
     end
 
     def <<(element)
@@ -47,22 +47,40 @@ module Aikido::Zen
     extend Forwardable
 
     def_delegators :@data,
-      :[], :fetch, :delete, :key?,
+      :delete, :key?,
       :each, :each_key, :each_value,
       :size, :empty?, :to_hash
 
     # @return [Integer]
     attr_reader :capacity
 
-    def initialize(capacity)
+    def initialize(capacity, mode: :fifo)
       raise ArgumentError, "cannot set capacity lower than 1: #{capacity}" if capacity < 1
+
+      unless [:fifo, :lru].include?(mode)
+        raise ArgumentError, "unsupported mode: #{mode}"
+      end
+
       @capacity = capacity
+      @mode = mode
+
       @data = {}
     end
 
     def []=(key, value)
       @data[key] = value
       @data.delete(@data.each_key.first) if @data.size > @capacity
+    end
+
+    def [](key)
+      @data[key] = @data.delete(key) if @mode == :lru && key?(key)
+      @data[key]
+    end
+
+    def fetch(key, ...)
+      return self[key] if key?(key)
+
+      @data.fetch(key, ...)
     end
   end
 end
--- test/aikido/zen/capped_collections_test.rb
+++ test/aikido/zen/capped_collections_test.rb
@@ -64,7 +64,7 @@ class Aikido::Zen::CappedCollectionsTest < ActiveSupport::TestCase
       assert_equal ["x", "y"], set.to_a
     end
 
-    test "overflowing the set removes the oldest element first" do
+    test "overflowing the set removes the lead recently added element first" do
       set = Aikido::Zen::CappedSet.new(3)
 
       set << 1 << 2 << 3
@@ -163,62 +163,90 @@ class Aikido::Zen::CappedCollectionsTest < ActiveSupport::TestCase
     test "preserves hash semantics" do
       map = Aikido::Zen::CappedMap.new(3)
 
-      map[1] = true
-      map[2] = true
-      map[3] = false
-      map[2] = false
+      map[1] = :a
+      map[2] = :b
+      map[3] = :c
+      map[2] = :B
 
       assert_equal 3, map.size
 
-      assert_equal true, map[1]
-      assert_equal false, map[2]
-      assert_equal false, map[3]
+      assert_equal :a, map[1]
+      assert_equal :B, map[2]
+      assert_equal :c, map[3]
       assert_nil map[4]
 
-      assert_equal true, map.fetch(1)
-      assert_equal false, map.fetch(2)
-      assert_equal false, map.fetch(3)
+      assert_equal :a, map.fetch(1)
+      assert_equal :B, map.fetch(2)
+      assert_equal :c, map.fetch(3)
       assert_raise(KeyError) { map.fetch(4) }
       assert_nil map.fetch(4, nil)
 
-      assert_equal false, map.delete(2)
+      assert_equal :B, map.delete(2)
       assert_nil map[2]
       assert_raise(KeyError) { map.fetch(2) }
-      assert_equal true, map.fetch(2, true)
+      assert_equal :b, map.fetch(2, :b)
     end
 
     test "insertion order is preserved" do
       map = Aikido::Zen::CappedMap.new(3)
-      map[3] = false
-      map[2] = true
-      map[1] = true
-      map[2] = false
+      map[3] = :c
+      map[2] = :b
+      map[1] = :a
+      map[2] = :B
 
       assert_equal 3, map.size
-      assert_equal [[3, false], [2, false], [1, true]], map.to_a
-      assert_equal({3 => false, 2 => false, 1 => true}, map.to_h)
+      assert_equal [[3, :c], [2, :B], [1, :a]], map.to_a
+      assert_equal({3 => :c, 2 => :B, 1 => :a}, map.to_h)
     end
 
-    test "overflowing the map removes the oldest element first" do
+    test "overflowing the map removes the least recently added element first" do
       map = Aikido::Zen::CappedMap.new(3)
 
-      map[1] = true
-      map[2] = false
-      map[3] = true
-      assert_equal [[1, true], [2, false], [3, true]], map.to_a
-      assert_equal({1 => true, 2 => false, 3 => true}, map.to_h)
+      map[1] = :a
+      map[2] = :b
+      map[3] = :c
+      assert_equal [[1, :a], [2, :b], [3, :c]], map.to_a
+      assert_equal({1 => :a, 2 => :b, 3 => :c}, map.to_h)
 
-      map[4] = false
-      assert_equal [[2, false], [3, true], [4, false]], map.to_a
-      assert_equal({2 => false, 3 => true, 4 => false}, map.to_h)
+      map[4] = :d
+      assert_equal [[2, :b], [3, :c], [4, :d]], map.to_a
+      assert_equal({2 => :b, 3 => :c, 4 => :d}, map.to_h)
 
-      map[3] = false
-      assert_equal [[2, false], [3, false], [4, false]], map.to_a
-      assert_equal({2 => false, 3 => false, 4 => false}, map.to_h)
+      map[3] = :C
+      assert_equal [[2, :b], [3, :C], [4, :d]], map.to_a
+      assert_equal({2 => :b, 3 => :C, 4 => :d}, map.to_h)
 
-      map[1] = true
-      assert_equal [[3, false], [4, false], [1, true]], map.to_a
-      assert_equal({3 => false, 4 => false, 1 => true}, map.to_h)
+      map[1] = :a
+      assert_equal [[3, :C], [4, :d], [1, :a]], map.to_a
+      assert_equal({3 => :C, 4 => :d, 1 => :a}, map.to_h)
+    end
+
+    test "overflowing the map removes the least recently used element first in :lru mode" do
+      map = Aikido::Zen::CappedMap.new(3, mode: :lru)
+
+      map[1] = :a
+      map[2] = :b
+      map[3] = :c
+      assert_equal [[1, :a], [2, :b], [3, :c]], map.to_a
+      assert_equal({1 => :a, 2 => :b, 3 => :c}, map.to_h)
+
+      map[4] = :d
+      assert_equal [[2, :b], [3, :c], [4, :d]], map.to_a
+      assert_equal({2 => :b, 3 => :c, 4 => :d}, map.to_h)
+
+      map[3] = :C
+      assert_equal [[2, :b], [3, :C], [4, :d]], map.to_a
+      assert_equal({2 => :b, 3 => :C, 4 => :d}, map.to_h)
+
+      # Simulate use map[2] so that map[3] and not map[2] is not removed next.
+      map[2]
+
+      # Simulate use map[3] so that map[4] and not map[3] is not removed next.
+      map.fetch(3, nil)
+
+      map[1] = :a
+      assert_equal [[2, :b], [3, :C], [1, :a]], map.to_a
+      assert_equal({2 => :b, 3 => :C, 1 => :a}, map.to_h)
     end
   end
 end
