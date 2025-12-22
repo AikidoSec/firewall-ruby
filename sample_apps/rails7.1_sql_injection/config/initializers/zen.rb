d0542f3525014701fb3e49caa71b20ff4172a4ba
Mark Smith <mark@aikido.dev>
2025-12-22 15:05:21 +0100

    WIP Anchor middleware to ActionDispatch::RemoteIp

--- sample_apps/rails7.1_sql_injection/config/initializers/zen.rb
+++ sample_apps/rails7.1_sql_injection/config/initializers/zen.rb
@@ -3,5 +3,5 @@
 if Rails.application.config.respond_to?(:zen)
   Rails.application.config.zen.blocking_mode = true
   Rails.application.config.zen.logger = ::Rails.logger.tagged("aikido")
-  Rails.application.config.zen.insert_middleware_after = ::Rails::Rack::Logger
+  Rails.application.config.zen.insert_middleware_after = ::ActionDispatch::RemoteIp
 end
