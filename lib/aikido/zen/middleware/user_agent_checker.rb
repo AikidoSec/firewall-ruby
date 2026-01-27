288e794976f2422ad0b65ce10355d8f34289af43
Mark Smith <mark@aikido.dev>
2026-01-27 15:59:24 +0100

    Normalize simple bypass comment

--- lib/aikido/zen/middleware/user_agent_checker.rb
+++ lib/aikido/zen/middleware/user_agent_checker.rb
@@ -33,7 +33,7 @@ module Aikido::Zen
       end
 
       def bypassed?(request)
-        # Bypass bot blocking and monitoring for allowed IPs
+        # Bypass for allowed IPs
         @settings.allowed_ips.include?(request.ip)
       end
     end
