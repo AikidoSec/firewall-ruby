6d1b2a6ab9cf2f1053b57a04e5e9091cedd469dd
Mark Smith <mark@aikido.dev>
2026-03-23 17:04:50 +0100

    Optimize regular expression
    for detecting numbers and comma-separated list of numbers.

--- lib/aikido/zen/scanners/sql_injection_scanner.rb
+++ lib/aikido/zen/scanners/sql_injection_scanner.rb
@@ -68,7 +68,7 @@ module Aikido::Zen
         return false if /\A[[:alnum:]_]+\z/i.match?(@input)
 
         # If the input is a comma-separated list of numbers, ignore it.
-        return false if /\A(?:\d+(?:,\s*)?)+\z/i.match?(@input)
+        return false if /\A[\s,]*\d[\s,\d]*\z/.match?(@input)
 
         Internals.detect_sql_injection(@query, @input, @dialect)
       rescue => err
