ba980bad719328cfb7af0d84481a68b1e5661386
Mark Smith <mark@aikido.dev>
2026-04-16 17:26:39 +0200

    Separate IPList from IPSet implementation

--- lib/aikido/zen/runtime_settings/ip_list.rb
+++ lib/aikido/zen/runtime_settings/ip_list.rb
@@ -12,7 +12,7 @@ module Aikido::Zen
         key: data["key"],
         source: data["source"],
         description: data["description"],
-        ips: RuntimeSettings::IPSet.from_json(data["ips"])
+        ips: Array(data["ips"]).map { |ip| IPAddr.new(ip) }
       )
     end
 
@@ -28,7 +28,29 @@ module Aikido::Zen
     end
 
     def include?(ip)
-      @ips.include?(ip)
+      native_ip = nativize_ip(ip)
+      return false if native_ip.nil?
+
+      @ips.any? { |pattern| pattern === native_ip }
+    end
+
+    private
+
+    def nativize_ip(ip)
+      case ip
+      when IPAddr
+        ip.native
+      when String
+        begin
+          IPAddr.new(ip).native
+        rescue IPAddr::InvalidAddressError
+          nil
+        end
+      when nil
+        nil
+      else
+        raise ArgumentError, "no explicit conversion of #{ip.class} to IPAddr"
+      end
     end
   end
 end
