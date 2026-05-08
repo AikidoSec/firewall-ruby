cad18110f0e42ab9e43e58f196a49ccf67c37d0e
Mark Smith <mark@aikido.dev>
2026-05-08 12:16:23 +0200

    Add e2e tests detects SQL injection in body with content types

--- test/e2e/rails7.2_generic/app/controllers/api/v1/profiles_controller.rb
+++ test/e2e/rails7.2_generic/app/controllers/api/v1/profiles_controller.rb
@@ -12,6 +12,17 @@ module Api
         )
         render json: results.rows
       end
+
+      def update
+        token = cookies[:token]
+        return render json: {error: "No token cookie"}, status: :unauthorized unless token
+
+        results = ActiveRecord::Base.connection.exec_query(
+          # Trigger SQL injection detection with param value "NEW_SECRET', name='NEW_NAME"
+          "UPDATE users SET secret='NEW_SECRET', name='NEW_NAME' WHERE token='#{token}'"
+        )
+        render json: results.rows
+      end
     end
   end
 end
