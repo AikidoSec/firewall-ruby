c741d1f573e3ab26e2b836a9af15b943c8d672c9
Mark Smith <mark@aikido.dev>
2026-05-13 14:54:04 +0200

    Return Boolean from update settings methods
    To detect whether the update method completed.

--- lib/aikido/zen/runtime_settings.rb
+++ lib/aikido/zen/runtime_settings.rb
@@ -86,7 +86,7 @@ module Aikido::Zen
     #
     # @param data [Hash] the decoded JSON payload from the /api/runtime/config
     #   API endpoint.
-    # @return [bool]
+    # @return [Boolean]
     def update_from_runtime_config_json(data)
       last_updated_at = updated_at
 
@@ -111,7 +111,7 @@ module Aikido::Zen
     #
     # @param data [Hash] the decoded JSON payload from the /api/runtime/firewall/lists
     #   API endpoint.
-    # @return [void]
+    # @return [Boolean]
     def update_from_runtime_firewall_lists_json(data)
       @blocked_user_agent_regexp = pattern(data["blockedUserAgents"])
 
@@ -148,6 +148,8 @@ module Aikido::Zen
       data["monitoredIPAddresses"]&.each do |ip_list|
         @monitored_ip_lists << RuntimeSettings::IPList.from_json(ip_list)
       end
+
+      true
     end
 
     # Construct a regular expression from the non-nil and non-empty string,
