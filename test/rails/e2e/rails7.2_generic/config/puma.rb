54cdaab1d9692262cf7c492fe9e2d1bf107652b1
Mark Smith <mark@aikido.dev>
2026-05-29 10:58:11 +0200

    Force forking

--- test/rails/e2e/rails7.2_generic/config/puma.rb
+++ test/rails/e2e/rails7.2_generic/config/puma.rb
@@ -23,6 +23,17 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
+workers ENV.fetch("WEB_CONCURRENCY", 2)
+preload_app!
+
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
+end
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
 
