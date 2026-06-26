dd17819961a18ea14aff52f96ed3986a4494289f
Mark Smith <mark@aikido.dev>
2026-06-26 10:38:18 +0200

    Fix predicate

--- lib/aikido/zen/scanners/path_traversal_scanner.rb
+++ lib/aikido/zen/scanners/path_traversal_scanner.rb
@@ -46,7 +46,7 @@ module Aikido::Zen
 
       def attack?
         # We block home-relative path
-        return true if @filepath.start_with?("~") && @filepath.include?(@input)
+        return true if @input.start_with?("~") && @filepath.start_with?(@input)
 
         # Single character are ignored because they don't pose a big threat
         return false if @input.length <= 1
