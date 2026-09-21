fdcb96924e69388b40924ba4984b9563cb87b1f1
Mark Smith <mark@aikido.dev>
2026-09-21 15:33:21 +0200

    Rename Agent#track_custom_event
    To report_custom_event.

--- lib/aikido/zen.rb
+++ lib/aikido/zen.rb
@@ -302,7 +302,7 @@ module Aikido
         user: context.request.actor
       )
 
-      agent.track_custom_event(event)
+      agent.report_custom_event(event)
     end
 
     # @return [Aikido::Zen::AttackWave::Detector] the attack wave detector.
